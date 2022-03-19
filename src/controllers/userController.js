0ed8908e973201903d9d03e1b810c30861271552
kimseongkyeong <isd3554@naver.com>
2022-03-19 21:17:43 +0900

    first commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,40 @@
 import res from "express/lib/response";
-export const trending = (req, res) => res.render("home",{ webtitle:"Home"});
-export const join = (req, res) => res.render("home");
+
+
+
+export const trending = (req,res) => {
+    const videos = [
+        {
+          title: "Hello",
+          title: "First Video",
+          rating: 5,
+          comments: 2,
+          createdAt: "2 minutes ago",
+          views: 59,
+          id: 1,
+        },
+        {
+          title: "Video #2",
+          title: "Second Video",
+          rating: 5,
+          comments: 2,
+          createdAt: "2 minutes ago",
+          views: 59,
+          id: 1,
+        },
+        {
+          title: "Whatsup",
+          title: "Third Video",
+          rating: 5,
+          comments: 2,
+          createdAt: "2 minutes ago",
+          views: 59,
+          id: 1,
+        },
+      ];
+    return res.render("home",{webtitle:"Main",videos});
+}
+export const join = (req, res) => res.render("join");
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const userParam = (req,res) => res.send(`parameter# ${req.params.aa} fgggggg`);
